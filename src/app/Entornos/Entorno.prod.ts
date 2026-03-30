5ffc0bf1579c0fe73cb8170633046d46390e3eb0
prosystem-pro <prosystem490@gmail.com>
2026-03-29 19:21:29 -0600

    Se integró la API correcta

--- src/app/Entornos/Entorno.prod.ts
+++ src/app/Entornos/Entorno.prod.ts
@@ -1,5 +1,5 @@
 export const Entorno = {
   Produccion: true,
-  ApiUrl: 'https://carritoweb-promesadedios-api-production.up.railway.app/api/',
+  ApiUrl: 'https://sastreria-confeccionescreateli-api-production.up.railway.app/api/',
   NombreEmpresa: 'ConfeccionesCreateli'
 };

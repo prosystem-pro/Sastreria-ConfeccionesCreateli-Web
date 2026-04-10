d93f468036fca85662fde315584436675ff5d6d7
prosystem-pro <prosystem490@gmail.com>
2026-04-10 02:23:55 -0600

    Impresion prueba 12

--- src/app/Paginas/Inicio/Ventas/venta-impresion/venta-impresion.component.ts
+++ src/app/Paginas/Inicio/Ventas/venta-impresion/venta-impresion.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { VentaServicio } from '../../../../Servicios/VentaServicio';
 import { AlertaServicio } from '../../../../Servicios/Alerta-Servicio';
 import { FormsModule } from '@angular/forms';
@@ -12,106 +12,40 @@ import { CommonModule } from '@angular/common';
   styleUrl: './venta-impresion.component.css'
 })
 export class VentaImpresionComponent implements OnInit {
-
-  datosImpresion: any;
-  Procesando = true;
+datosImpresion: any;
+  Procesando = false;
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router,
     private VentaServicio: VentaServicio,
     private AlertaServicio: AlertaServicio
-  ) {}
+  ) { }
 
   ngOnInit() {
-
     const codigoPedido = this.route.snapshot.paramMap.get('codigoPedido');
-
     if (codigoPedido) {
       this.CargarDatosImpresion(Number(codigoPedido));
-    } else {
-      this.router.navigate(['/venta-listado']);
     }
-
   }
 
   CargarDatosImpresion(codigoPedido: number) {
-
     this.Procesando = true;
+    this.VentaServicio.ObtenerDatosImpresionVenta(codigoPedido).subscribe({
+      next: (resp) => {
+        console.log('DATOS',resp)
+        this.datosImpresion = resp.data;
+        this.Procesando = false;
 
-    this.VentaServicio
-      .ObtenerDatosImpresionVenta(codigoPedido)
-      .subscribe({
-
-        next: (resp) => {
-
-          this.datosImpresion = resp.data;
-
-          setTimeout(() => {
-            this.Imprimir();
-          }, 600);
-
-        },
-
-        error: (err) => {
-
-          this.Procesando = false;
-
-          this.AlertaServicio.MostrarError(
-            'Error al cargar la factura'
-          );
-
-          console.error(err);
-
-          this.router.navigate(['/venta-listado']);
-
-        }
-
-      });
-
+        // Lanza la impresión automáticamente
+        setTimeout(() => {
+          window.print();
+        }, 500); // espera a que Angular renderice
+      },
+      error: (err) => {
+        this.Procesando = false;
+        this.AlertaServicio.MostrarError('Error al cargar la factura');
+        console.error(err);
+      }
+    });
   }
-
-  Imprimir() {
-
-    const contenido = document.getElementById('ticket-impresion');
-
-    if (!contenido) {
-      this.Procesando = false;
-      return;
-    }
-
-    const ventana = window.open('', '', 'width=600,height=800');
-
-    if (!ventana) {
-      this.Procesando = false;
-      return;
-    }
-
-    ventana.document.write(`
-      <html>
-        <head>
-          <title>Impresion</title>
-        </head>
-        <body>
-          ${contenido.innerHTML}
-        </body>
-      </html>
-    `);
-
-    ventana.document.close();
-    ventana.focus();
-
-    setTimeout(() => {
-
-      ventana.print();
-      ventana.close();
-
-      this.Procesando = false;
-
-      this.router.navigate(['/venta-listado']);
-
-    }, 500);
-
-  }
-
 }

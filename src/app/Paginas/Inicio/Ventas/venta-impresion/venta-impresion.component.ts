3ee722e67cc1c1bf2a4365f7798414b06b50b1e7
prosystem-pro <prosystem490@gmail.com>
2026-04-10 00:36:59 -0600

    Impresion prueba 6

--- src/app/Paginas/Inicio/Ventas/venta-impresion/venta-impresion.component.ts
+++ src/app/Paginas/Inicio/Ventas/venta-impresion/venta-impresion.component.ts
@@ -12,8 +12,10 @@ import { CommonModule } from '@angular/common';
   styleUrl: './venta-impresion.component.css'
 })
 export class VentaImpresionComponent implements OnInit {
-datosImpresion: any;
+
+  datosImpresion: any;
   Procesando = false;
+  esIOS = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,30 +24,50 @@ datosImpresion: any;
   ) { }
 
   ngOnInit() {
+
+    // Detectar iPhone/iPad
+    this.esIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+
     const codigoPedido = this.route.snapshot.paramMap.get('codigoPedido');
+
     if (codigoPedido) {
       this.CargarDatosImpresion(Number(codigoPedido));
     }
   }
 
   CargarDatosImpresion(codigoPedido: number) {
+
     this.Procesando = true;
+
     this.VentaServicio.ObtenerDatosImpresionVenta(codigoPedido).subscribe({
+
       next: (resp) => {
-        console.log('DATOS',resp)
+
+        console.log('DATOS', resp);
+
         this.datosImpresion = resp.data;
         this.Procesando = false;
 
-        // Lanza la impresión automáticamente
-        setTimeout(() => {
-          window.print();
-        }, 500); // espera a que Angular renderice
+        // Android y Desktop imprimen automático
+        if (!this.esIOS) {
+          setTimeout(() => {
+            window.print();
+          }, 800);
+        }
       },
+
       error: (err) => {
+
         this.Procesando = false;
         this.AlertaServicio.MostrarError('Error al cargar la factura');
         console.error(err);
+
       }
+
     });
+  }
+
+  Imprimir() {
+    window.print();
   }
 }
